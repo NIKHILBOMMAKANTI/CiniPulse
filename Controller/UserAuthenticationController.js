43004ede9764b83f4acc2a21a8c7d23c3e44650d
NIKHILBOMMAKANTI <nikhilbommakanti2001@gmail.com>
2025-07-01 18:46:35 +0530

    Changes related to redirection

--- Controller/UserAuthenticationController.js
+++ Controller/UserAuthenticationController.js
@@ -69,13 +69,13 @@ const roleBasedAccess = (req, res) => {
     return res.status(200).json({
       message: "Access Granted",
       id: req.user_data._id,
-      redirectTo: "/client/index.html",
+      redirectTo: "/index.html",
     });
   } else {
     return res.status(200).json({
       message: "Access Granted",
       id: req.user_data._id,
-      redirectTo: "/client/Admin.html",
+      redirectTo: "/Admin.html",
     });
   }
 
